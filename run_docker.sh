6c2ca77cb4d06c4385befaa69176db047383c0bb
hieugiddy <59432500+hieugiddy@users.noreply.github.com>
2023-05-25 21:31:09 +0700

    Delete run_docker.sh

--- run_docker.sh
+++
@@ -1,9 +0,0 @@
-# Step 1:
-# Build image and add a descriptive tag
-docker build --tag=hieudm17-capstone .
-# Step 2: 
-# List docker images
-docker images list
-# Step 3: 
-# Run flask app
-docker run -p 8000:80 hieudm17-capstone
